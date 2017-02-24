31040cedf017735061bc2609d5818663667f0243
John Ford <john@johnford.org>
2017-02-24 20:08:28 +0100

    Tests for generating requests to run remotely

--- test-src/test_S3.js
+++ test-src/test_S3.js
@@ -154,5 +154,52 @@ describe('S3 Client', () => {
     });
   });
 
+  describe('Generate Multipart Request', () => {
+    it ('should return the right values', async () => {
+      let {inst} = runner();
+      let parts = [
+        {sha256: crypto.createHash('sha256').update('part1').digest('hex'), size: 1024},
+        {sha256: crypto.createHash('sha256').update('part2').digest('hex'), size: 1024},
+        {sha256: crypto.createHash('sha256').update('part3').digest('hex'), size: 128},
+      ];
+      let result = await inst.generateMultipartRequest('example-bucket', 'example-key', 'example-uploadid', parts);
+
+      let n = 1;
+      for (let request of result) {
+        assume(request).has.property('url', `https://example-bucket.s3.amazonaws.com/example-key?partNumber=${n}&uploadId=example-uploadid`);
+        assume(request).has.property('method', 'PUT');
+        assume(request).has.property('headers');
+        assume(request.headers).has.property('x-amz-content-sha256', parts[n-1].sha256);
+        assume(request.headers).has.property('content-length', parts[n-1].size);
+        assume(request.headers).has.property('Host', 'example-bucket.s3.amazonaws.com');
+        assume(request.headers).has.property('Authorization');
+        assume(request.headers).has.property('X-Amz-Date');
+        n++;
+      }
+
+    });
+  });
+
+  describe('Generate Single Part Request', () => {
+    it ('should return the right values', async () => {
+      let {inst} = runner();
+      let sha256 = crypto.createHash('sha256').update('part1').digest('hex');
+      let size = 1024;
+
+      let result = await inst.generateSinglepartRequest('example-bucket', 'example-key', size, sha256);
+      
+      console.log(result);
+      assume(result).has.property('url', `https://example-bucket.s3.amazonaws.com/example-key`);
+      assume(result).has.property('method', 'PUT');
+      assume(result).has.property('headers');
+      assume(result.headers).has.property('x-amz-content-sha256', sha256);
+      assume(result.headers).has.property('x-amz-meta-taskcluster-content-sha256', sha256);
+      assume(result.headers).has.property('x-amz-meta-taskcluster-content-length', size);
+      assume(result.headers).has.property('content-length', size);
+      assume(result.headers).has.property('Host', 'example-bucket.s3.amazonaws.com');
+      assume(result.headers).has.property('Authorization');
+      assume(result.headers).has.property('X-Amz-Date');
+    });
+  });
 });
 
