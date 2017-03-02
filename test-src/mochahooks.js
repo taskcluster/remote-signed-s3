1e7ba01a1898c3b7e88471c87b571549f19e632c
John Ford <john@johnford.org>
2017-03-02 23:14:51 +0100

    Create bigfile if not there

---
+++ test-src/mochahooks.js
@@ -0,0 +1,12 @@
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+var fs = require('fs');
+
+if (!fs.existsSync(__dirname + '/../bigfile')) {
+  var spawn = require('child_process').spawnSync;
+  spawn('dd', ['if=/dev/urandom', 'of=' + __dirname + '/../bigfile', 'bs=' + 1024*1024, 'count=500'], {
+    stdio: [0, 1, 2],
+  });
+}
